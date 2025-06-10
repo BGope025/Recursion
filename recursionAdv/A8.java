5eee21f7206ff02d4c23c4c90519c0307a6fe6c1
BGope025 <bgope025@gmail.com>
2025-06-10 21:49:33 +0530

    adding new question

--- recursionAdv/A8.java
+++ recursionAdv/A8.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
-
+// Sudoku solver
 public class A8 {
+    public boolean helper(char[][] board, int row, int col) {
+        int r=0, c=0;
+        if (col!= board.length) {
+            r=row;
+            c=col+1;
+        }
+        else {
+            r=row+1;
+            c=0;
+        }
+        if (board[row][col]!='-') {
+            if (helper(board, r, c))
+                return true;
+        }
+        else {
+            for (int i=1;i<=9;i++) {
+                if (isSafe(board, row, col, i)) {
+                    board[row][col]=(char)(i+'0');
+                    if (helper(board, r, c))
+                        return true;
+                    else
+                }
+            }
+        }
+    }
+    public void solveSudoku(char[][] board) {
+        helper(board, 0, 0);
+    }
 }
